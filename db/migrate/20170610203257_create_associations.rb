3ca34bbaad97f63a0193a5bb63c50a6e46ff44bc
Maja Kędras <r2d2ka@gmail.com>
2017-06-10 22:35:03 +0200

    Added Meal and Order models

---
+++ db/migrate/20170610203257_create_associations.rb
@@ -0,0 +1,6 @@
+class CreateAssociations < ActiveRecord::Migration[5.0]
+  def change
+    create_table :associations do |t|
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170610180152) do
+ActiveRecord::Schema.define(version: 20170610203257) do
+
+  create_table "associations", force: :cascade do |t|
+  end
 
   create_table "identities", force: :cascade do |t|
     t.integer  "user_id"
@@ -19,6 +22,19 @@ ActiveRecord::Schema.define(version: 20170610180152) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_identities_on_user_id"
+  end
+
+  create_table "meals", force: :cascade do |t|
+    t.string   "name"
+    t.decimal  "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.string   "restaurant_name"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
   create_table "users", force: :cascade do |t|
